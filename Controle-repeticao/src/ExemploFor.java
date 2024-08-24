1296bbae76f4903dfb18d6a59872e53b63a2ac2b
Marcos Paulo <mpssss@hotmail.com>
2024-08-24 18:27:01 -0300

    Iniciando estudos pelo For

---
+++ Controle-repeticao/src/ExemploFor.java
@@ -0,0 +1,11 @@
+public class ExemploFor {
+
+    public static void main(String[] args) {
+
+        for (int contador = 2; contador <= 20; contador++) {
+            System.out.println("Contador :" + contador);
+        }
+
+    }
+
+}
