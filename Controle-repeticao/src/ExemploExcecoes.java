83da11968501699eb2c24be3e67ab1b9d61ab316
Marcos Paulo <mpssss@hotmail.com>
2024-08-28 21:54:27 -0300

    Estudo sobre Excecoes

---
+++ Controle-repeticao/src/ExemploExcecoes.java
@@ -0,0 +1,33 @@
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class ExemploExcecoes {
+
+    public static void main(String[] args) {
+        try {
+            Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+
+            System.out.println("Digite seu nome");
+            String nome = sc.next();
+
+            System.out.println("Digite seu sobrenome");
+            String sobrenome = sc.next();
+
+            System.out.println("Digite sua idade");
+            int idade = sc.nextInt();
+
+            System.out.println("Digite sua altura");
+            Double altura = sc.nextDouble();
+
+            System.out.println("Ola, me Chamo " + nome.toUpperCase() + "");
+            System.out.println("Tenho" + idade + "anos");
+            System.out.println("Minha altura é " + altura + "cm");
+
+            sc.close();
+        } catch (InputMismatchException e) {
+            System.out.println("O campo idade e altura precisam ser numericos, não adicionar virgulas.");
+        }
+    }
+
+}
